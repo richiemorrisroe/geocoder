d78829587086e680d14e66667eb52b8d32513488
Richie Morrisroe <richie.morrisroe@gmail.com>
2021-01-01 15:38:48 +0000

    remove characterisation tests for now

--- geocode/tests/test_google.py
+++ geocode/tests/test_google.py
@@ -12,19 +12,19 @@ def test_get_api_key() -> None:
 
 from geocode_funcs import get_google_results, get_api_key, read_results_from_pickle
 key = get_api_key('key.txt')
-def test_nonfull_results():
-    kilcanway_nonfull = get_google_results("Kilcanway, Mallow, Co. Cork, Ireland",
-                                           api_key = key,
-                                           return_full_response = False)
-    oldnon_full = read_results_from_pickle('nonfull_kilcanway.pkl')
-    assert kilcanway_nonfull == oldnon_full
+# def test_nonfull_results():
+#     kilcanway_nonfull = get_google_results("Kilcanway, Mallow, Co. Cork, Ireland",
+#                                            api_key = key,
+#                                            return_full_response = False)
+#     oldnon_full = read_results_from_pickle('nonfull_kilcanway.pkl')
+#     assert kilcanway_nonfull == oldnon_full
 
-def test_full_results():
-    kilcanway_full = get_google_results("Kilcanway, Mallow, Co. Cork, Ireland",
-                                           api_key = key,
-                                           return_full_response = True)
-    old_full = read_results_from_pickle('full_kilcanway.pkl')
-    assert kilcanway_full == old_full
+# def test_full_results():
+#     kilcanway_full = get_google_results("Kilcanway, Mallow, Co. Cork, Ireland",
+#                                            api_key = key,
+#                                            return_full_response = True)
+#     old_full = read_results_from_pickle('full_kilcanway.pkl')
+#     assert kilcanway_full == old_full
 
 import pandas as pd
 from pathlib import Path
