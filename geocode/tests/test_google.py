e6cb63967cd19b71dba5f018e830502eafa23606
richie <richie.morrisroe@gmail.com>
2022-08-27 15:04:02 +0100

    update tests to take input_data arg

--- geocode/tests/test_google.py
+++ geocode/tests/test_google.py
@@ -30,12 +30,13 @@ import pandas as pd
 from pathlib import Path
 import os
 from geocode.geocode_funcs import create_logger, log_progress_and_results
-resource_path = "/home/richie/geocoder" 
+resource_path = "/home/richie/Dropbox/for_dropbox/geocoder/" 
 output_data = pd.read_csv(os.path.join(resource_path, 'output_full_2018_19.csv'))
 output_data_100 = output_data.iloc[0:100,]
 output_data_500 = output_data.iloc[0:500,]
 output_data_10k = output_data
 output_filename = "test_output_file.csv"
+
 logger = create_logger()
 
 
@@ -45,7 +46,7 @@ def test_output_data_exists():
 
 def test_output_length_100(caplog) -> None:
     log_progress_and_results(output_data_100, logger, output_data_100.input_string,
-                             output_filename)
+                             output_filename, input_data_sample)
     assert 'Completed 100 of 100 address' in caplog.text
 
 
@@ -53,14 +54,15 @@ def test_output_length_500() -> None:
     log_progress_and_results(output_data_500,
                              logger,
                              output_data_500.input_string,
-                             output_filename)
+                             output_filename, input_data_sample)
     assert os.path.exists(output_filename + '_bak')
 
 def test_output_length_10000() -> None:
     log_progress_and_results(output_data_10k,
                              logger,
                              output_data_10k.input_string,
-                             output_filename)
+                             output_filename,
+                             input_data_sample)
     assert os.path.exists(output_filename)
 
 from geocode.join import join_input_and_output, preprocess_raw_data_for_join, add_ireland_to_address
